76b95c5eea5cdda8f73b9e58b14fce15b0c0876b
Gagan <gkaur21.rrc.ca>
2025-04-03 19:51:55 -0500

    Completed debugging steps.

--- user_interface/account_details_window.py
+++ user_interface/account_details_window.py
@@ -6,7 +6,7 @@ from ui_superclasses.details_window import DetailsWindow
 from PySide6.QtWidgets import QMessageBox
 from PySide6.QtCore import Signal
 from bank_account.bank_account import BankAccount
-import copy
+from copy import copy
 
 class AccountDetailsWindow(DetailsWindow):
     """
@@ -21,3 +21,21 @@ class AccountDetailsWindow(DetailsWindow):
             None
         """
         super().__init__()
+        
+        if isinstance(account, BankAccount):
+            self.account = copy(account)
+            self.account_number_label.setText(str(self.account.account_number))
+            self.balance_label.setText(f"${self.account.balance:.2f}")
+            
+            self.deposit_button.clicked.connect(self.__on_apply_transaction)
+            self.withdraw_button.clicked.connect(self.__on_apply_transaction)
+            self.exit_button.clicked.connect(self.__on_exit)
+        else:
+            self.reject()
+        
+        
+    def __on_apply_transaction(self):
+        pass
+
+    def __on_exit(self):
+        pass
